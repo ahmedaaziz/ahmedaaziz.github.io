a0782bd790b535ba598058c207b4d510860e90ea
ahmed0abdelaziz@gmail.com <ahmed0abdelaziz@gmail.com>
2021-07-07 19:51:42 +0200

    initial

--- gulpfile.js
+++ gulpfile.js
@@ -1,64 +1,77 @@
-// defining packages
+"use strict";
+var gulp = require('gulp'),
+    del = require('del'),
+    sass = require('gulp-sass'),
+    browserSync = require('browser-sync').create(),
+    concat = require('gulp-concat'),
+    deporder = require('gulp-deporder'),
+    terser = require('gulp-terser'),
+    imagemin = require('gulp-imagemin'),
+    { series, parallel } = require('gulp');
+  
 
-const gulp = require('gulp');
-const sass = require('gulp-sass');
-const browserSync = require('browser-sync').create();
-const minify = require('gulp-minify');
-const deploy = require('gulp-gh-pages');
-// const options = { 
-//     remoteUrl: "https://github.com/ahmedaaziz/ahmedaaziz.github.io.git",
-//     branch: "master"};
-
-
-// tasks
-
-    /** Compile scss to css **/
-
-function style() {
-    //1.where is my scss file
-    return gulp.src('./src/scss/**/*.scss')
-    //2.pass file through sass compiler
+// Compile Scss into Css
+function style(){
+    //where is my scss file 
+    return gulp.src('./assets/scss/*.scss')
+    // pas that file  through sass compiler 
+    .pipe(sass().on('error',sass.logError))
     .pipe(sass())
-    //3.where do i save the compiled css
-    .pipe(gulp.dest('./dist/css'))
-    //4.stream changes to all broswer
+    //where i do save my files 
+    .pipe(gulp.dest('./dist/assets/css'))
+    // stream changes to all browser
     .pipe(browserSync.stream());
 }
 
-  gulp.task('deploy', function () {
-    return gulp.src("./src/**/*")
-      .pipe(deploy({ 
-        remoteUrl: "https://github.com/ahmedaaziz/ahmedaaziz.github.io.git",
-        branch: "master"
-      }))
+// Minify Images
+function minifyImages(){
+  return gulp.src('./assets/images/**/**')
+        .pipe(imagemin())
+        .pipe(gulp.dest('./dist/assets/images'));
+
+}
+
+function js() {
+
+  return gulp.src('./assets/js/**/*')
+    .pipe(deporder())
+    .pipe(concat('main.js'))
+    .pipe(terser())
+    .pipe(gulp.dest('./dist/assets/js'));
+
+}
+
+
+// Clean task
+function clean(){
+    return del(['dist']);
+}
+
+// watch all changes on localhost
+function watch(){
+  browserSync.init({
+    server:{
+      baseDir: "./",
+      }
+    // port:3000,
+    // proxy: {
+    //   target: "localhost:8080/back-end/"
+    // }
   });
 
 
-// function deploy(){
-//   return gulp.src('./dist/**/*')
-//     .pipe(ghPages(options));
-
-// }
-
-function compress(){
-    return gulp.src('./src/js/**/*.js')
-        .pipe(minify())
-        .pipe(gulp.dest('./dist/js'))
-            .pipe(browserSync.stream());
-
+  gulp.watch('./assets/scss/**/*.scss', style);
+  gulp.watch('*.html').on('change',browserSync.reload);
+  gulp.watch('./assets/js/**/*.js',js);
+  gulp.watch('./assets/js/**/*.js').on('change',browserSync.reload);
+  gulp.watch('./assets/images/**/**',minifyImages);
+  gulp.watch('./assets/images/**/**').on('change',browserSync.reload);
+  
 }
 
-function watch() {
-    browserSync.init({
-        server: {
-            baseDir: './'
-        }
-    });
-    gulp.watch('./src/scss/**/*.scss',style);
-    gulp.watch('./src/js/**/*.js',compress).on('change',browserSync.reload);
-    gulp.watch('./*.html').on('change',browserSync.reload);
-}
 exports.style = style;
-exports.compress = compress;
+exports.minifyImages = minifyImages;
+exports.clean = clean;
+exports.js = js;
 exports.watch = watch;
-// exports.deploy = deploy;
+exports.default = series(parallel(style,minifyImages,js),watch);
